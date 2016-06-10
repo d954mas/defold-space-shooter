1f0544b21e8e8b2d90ace020eeabab48293b7135
Dima Popov <d954mas@gmail.com>
2016-06-10 21:42:04 +0300

    bullets

--- game/bullet/bullet.go
+++ game/bullet/bullet.go
@@ -1,8 +1,23 @@
+components {
+  id: "script"
+  component: "/game/bullet/bullet.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
-  id: "sprite2"
+  id: "sprite"
   type: "sprite"
   data: "tile_set: \"/background/background.atlas\"\n"
-  "default_animation: \"earthlike_planet\"\n"
+  "default_animation: \"gas_planet\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
--- game/hero/spaceship.go
+++ game/hero/spaceship.go
@@ -14,6 +14,23 @@ components {
   }
 }
 embedded_components {
+  id: "bullet_factory"
+  type: "factory"
+  data: "prototype: \"/game/bullet/bullet.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
