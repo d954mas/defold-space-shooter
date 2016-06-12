894c9d14b73ba0febf7b4f7686b4e1aafccd6fc9
Dima Popov <d954mas@gmail.com>
2016-06-12 23:26:57 +0300

    sounds

--- render/render_helper.lua
+++ render/render_helper.lua
@@ -10,6 +10,13 @@ M.world_height=1
 M.camera_x=0
 
 function M.action_to_position(action)
+	position = vmath.vector3((action.screen_x-M.viewport_x)*M.scale_width, (action.screen_y-M.viewport_y) * M.scale_height, 0)
+	position.x=M.clamp(position.x,0,720)
+	position.y=M.clamp(position.y,0,960)
+	return position
+end
+
+function M.action_to_camera_position(action)
 	position = vmath.vector3(M.camera_x+(action.screen_x-M.viewport_x)*M.scale_width, (action.screen_y-M.viewport_y) * M.scale_height, 0)
 	position.x=M.clamp(position.x,0,720)
 	position.y=M.clamp(position.y,0,960)
