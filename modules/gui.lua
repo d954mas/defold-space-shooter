05aca42e2b0385a1ba4eb368e76858ea477c21dd
Dima Popov <d954mas@gmail.com>
2016-06-14 01:45:02 +0300

    refactor checkbox

--- modules/gui.lua
+++ modules/gui.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+--read about OOP in lua
 function M.getCheckBox(checked,imageOn,imageOff,node)
 	local checkBox = {imageOn=imageOn, imageOff=imageOff,checked=true,node=node}
 	checkBox.setChecked = 
@@ -7,9 +8,20 @@ function M.getCheckBox(checked,imageOn,imageOff,node)
 			checkBox.checked=checked 
 			local animation=checkBox.imageOn
 			if(not checked)then animation=checkBox.imageOff end
-			print(animation)
 			gui.play_flipbook(node,animation)
+			if(not (checkBox.callback == nil))then
+				checkBox.callback(checkBox.checked)
+			end
 			return checkBox
+		end
+	checkBox.clicked = 
+		function(x,y)
+			if(gui.pick_node(node, x, y))then
+				checkBox.setChecked(not checkBox.checked)
+				return true
+			else
+				return false
+			end
 		end
 	checkBox.setChecked(checked)	
 	return checkBox		
