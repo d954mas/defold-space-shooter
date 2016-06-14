aca207996b4615ed3c8d80c44c7a801114717fff
Dima Popov <d954mas@gmail.com>
2016-06-14 20:32:50 +0300

    asteroid explosion and bullet perticle

--- game/bullet/bullet.go
+++ game/bullet/bullet.go
@@ -1,4 +1,19 @@
 components {
+  id: "particlefx"
+  component: "/game/bullet/bullet_tail.particlefx"
+  position {
+    x: 0.0
+    y: -19.109697
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
   id: "script"
   component: "/game/bullet/bullet.script"
   position {
